18878f0e08c92cef312603e22d3ccab9c08b80ce
koushik nag shuvo <60665707+koushiknagshuvo@users.noreply.github.com>
2023-11-18 22:34:12 +0600

    first commit

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index(Request $request)
+    {
+        $id = $request->id;
+        $name = "Koushik Nag";
+        $age = "26";
+
+        $data = [
+            "id" => "$id",
+            "name" => $name,
+            "age" => $age
+        ];
+
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $cookieMinutes = 1;
+        $cookiePath = '/';
+        $cookieDomain = $_SERVER['SERVER_NAME'];
+        $cookieSecure = false;
+        $cookieHttpOnly = true;
+
+        return response($data)->cookie($cookieName, $cookieValue, $cookieMinutes, $cookiePath, $cookieDomain, $cookieSecure, $cookieHttpOnly)->setStatusCode(200);
+    }
+}
